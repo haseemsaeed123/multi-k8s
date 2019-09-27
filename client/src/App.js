f0a16d4e70fd23fda53330d3a1ad1ea19df4fb69
Haseem Saeed <haseem.saeed@arpatech.com>
2019-09-27 09:57:40 +0500

    haseem changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fibs Calculator version KUBERNETES!</h1>
+            <h1 className="App-title">Fibsi Calculator version KUBERNETES!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
