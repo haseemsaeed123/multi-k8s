bbcf2cfe69dbb78c18292a2723b9844a2a18b261
Haseem Saeed <haseem.saeed@arpatech.com>
2019-09-27 12:24:38 +0500

    deleted everything

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fibsinnnii Calculator version KUBERNETES!</h1>
+            <h1 className="App-title">Fibsinn0oonii Calculator version KUBERNETES!</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
